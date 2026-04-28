c38b7b800da8334f174e7b3eb518fea74784aa3d
Thomas Vainikainen <ag2982@student.jamk.fi>
2026-04-28 15:28:49 +0300

    forgot to add backend on the previous commit

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ const port = parseInt(process.env.PORT || '3000', 10);
 app.use(cors());
 
 app.get('/api/test', (req, res) => {
-    res.json({ message: 'Hello from Backend!' });
+  res.json({ message: 'Hello from Backend!' });
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
