43601c405b2d7f3b1b9fbcaa58d528506f77fb8d
Jun Fengari <AG4042@student.jamk.fi>
2026-04-28 15:02:51 +0300

    working back+frontend version 1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
-app.use(cors()); // Enable CORS
-app.get('/', (req, res) => {
-  res.send('Hello from Backend!');
+
+const port = parseInt(process.env.PORT || '3000', 10);
+
+app.use(cors());
+
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Hello from Backend!' });
 });
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on port ${port}`);
 });
