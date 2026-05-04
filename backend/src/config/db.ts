ef5a59f2a127b0c2583b087e65f8b4dbe27c2ba6
Thomas Vainikainen <ag2982@student.jamk.fi>
2026-05-04 15:32:22 +0300

    started on f2b

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -11,36 +11,36 @@ These will go to the .env file and will be read from there
 
 import mysql from 'mysql2/promise';
 
-const db = mysql.createPool({
-    host: process.env.RDS_HOST,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    port: Number(process.env.RDS_PORT),
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-});
-
-async function testConnection() {
-    const connection = await db.getConnection();
-    console.log('✅ Database connected successfully');
-    connection.release();
-}
-
 // const db = mysql.createPool({
-//   host: 'localhost',
-//   port: Number(process.env.DB_PORT),
-//   user: process.env.DB_USER,
-//   password: process.env.MYSLI_PASSWORD,
-//   database: process.env.MYSLI_DB,
+//     host: process.env.RDS_HOST,
+//     user: process.env.RDS_USERNAME,
+//     password: process.env.RDS_PASSWORD,
+//     database: process.env.RDS_DB_NAME,
+//     port: Number(process.env.RDS_PORT),
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
 // });
 
 // async function testConnection() {
-//   const connection = await db.getConnection();
-//   console.log('✅ Database connected successfully');
-//   connection.release();
+//     const connection = await db.getConnection();
+//     console.log('✅ Database connected successfully');
+//     connection.release();
 // }
+
+const db = mysql.createPool({
+  host: 'localhost',
+  port: Number(process.env.RDS_PORT),
+  user: process.env.RDS_USERNAME,
+  password: process.env.MYSLI_PASSWORD,
+  database: process.env.MYSLI_DB,
+});
+
+async function testConnection() {
+  const connection = await db.getConnection();
+  console.log('✅ Database connected successfully');
+  connection.release();
+}
 
 testConnection();
 
