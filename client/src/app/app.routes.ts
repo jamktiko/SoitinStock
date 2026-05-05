de5272c6a9565340377383026fa03265a118d7f1
Thomas Vainikainen <ag2982@student.jamk.fi>
2026-05-05 12:51:44 +0300

    added renting component and routing to it

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { FrontPage } from './front-page/front-page';
+import { RentingForm } from './renting-form/renting-form';
 // import { Products } from './products/products';
 // import { Page } from './rentingForm/page';
 // import { NotFoundError } from 'rxjs';
@@ -8,5 +9,6 @@ export const routes: Routes = [
   { path: '', component: FrontPage },
   { path: 'products', component: FrontPage },
   { path: 'products/:category', component: FrontPage },
+  { path: 'renting', component: RentingForm },
   { path: '**', redirectTo: '' },
 ];
