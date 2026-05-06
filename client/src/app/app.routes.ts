fcaafd85dde5fb1520f789abf8005a6278d3f53e
Thomas Vainikainen <ag2982@student.jamk.fi>
2026-05-06 10:34:47 +0300

    login started

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,8 +4,10 @@ import { RentingForm } from './renting-form/renting-form';
 // import { Products } from './products/products';
 // import { Page } from './rentingForm/page';
 // import { NotFoundError } from 'rxjs';
+import { LoginForm } from './login-form/login-form';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginForm },
   { path: '', component: FrontPage },
   { path: 'products', component: FrontPage },
   { path: 'products/:category', component: FrontPage },
--- client/src/app/login-form/login-form.ts
+++ client/src/app/login-form/login-form.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-form',
-  imports: [],
-  templateUrl: './login-form.html',
-  styleUrl: './login-form.css',
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
 })
-export class LoginForm {
+export class LoginForm implements OnInit {
+  error = '';
+  // injektoidaan router ja authService
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+  ) {}
 
+  ngOnInit() {
+    // aina kun login-komponentti ladataan, poistetaan token
+    this.authService.logout();
+  }
+
+  // lomakkeen lähetys
+  onSubmit(formData: any) {
+    //console.log(formData);
+    // authServicen login-metodi palauttaa observablen, josta saadaan true tai false
+    this.authService.login(formData.tunnus, formData.salasana).subscribe((result) => {
+      if (result === true) {
+        // true päästää admin-komponenttiin
+        this.router.navigate(['/admin']);
+      } else {
+        this.error = 'Tunnus tai salasana väärä';
+      }
+    });
+  }
 }
