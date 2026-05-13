a33c604b95d33b1e898eb82f0a6c818f0c7d88b7
Thomas Vainikainen <ag2982@student.jamk.fi>
2026-05-13 13:18:22 +0300

    image routing and loading in modal

---
+++ client/src/app/.models/imageMap.ts
@@ -0,0 +1,8 @@
+export const image_map: Record<number, string> = {
+  1: '/pics/guitar.png',
+  2: '/pics/piano.png',
+  3: '/pics/violin.png',
+  4: '/pics/drums.png',
+  5: '/pics/harps.png',
+  6: '/pics/accordion.png',
+};
